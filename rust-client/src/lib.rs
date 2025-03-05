6f1cb887c0f55bda3b906984e4114cdb3e6d78aa
Garret Rieger <grieger@google.com>
2025-03-05 00:54:13 +0000

    WIP rust client implementation.

--- rust-client/src/lib.rs
+++ rust-client/src/lib.rs
@@ -1,6 +1,8 @@
 mod utils;
 
 use wasm_bindgen::prelude::*;
+use wasm_bindgen_futures::JsFuture;
+use web_sys::{Request, RequestInit, RequestMode, Response};
 
 #[wasm_bindgen]
 extern "C" {
@@ -10,24 +12,36 @@ extern "C" {
 #[wasm_bindgen]
 struct IftState {
     font_subset: Vec<u8>,
+    font_url: String,
+    status: Status,
+    pending_callbacks: Vec<js_sys::Function>,
+    // TODO store requested codepoints, features, design space.
+    // TODO store loaded patch data by url
+}
+
+enum Status {
+    Uninitialized,
+    LoadingInitFont,
+    Ready,
+    Extending,
+    Error,
 }
 
 #[wasm_bindgen]
 impl IftState {
-    pub fn new(font_url: &str) -> Self {
-        // TODO: implement me!
+    pub fn new(font_url: String) -> Self {
         Self {
-            font_subset: vec![1, 2, 3, 4],
+            font_subset: vec![],
+            font_url,
+            status: Status::Uninitialized,
+            pending_callbacks: vec![],
         }
     }
 
-    pub fn extend(&self, codepoints: &[u32], callback: &js_sys::Function) {
-        if !codepoints.is_empty() {
-            let this = JsValue::null();
-            let v = JsValue::from(codepoints[0]);
-            callback.call1(&this, &v);
-        }
-        // TODO: implement me!
+    pub fn extend(&mut self, codepoints: &[u32], callback: js_sys::Function) {
+        self.pending_callbacks.push(callback);
+        // TODO: update codepoint list.
+        self.process();
     }
 
     pub fn font_size(&self) -> usize {
@@ -36,5 +50,24 @@ impl IftState {
 
     pub fn font_ptr(&self) -> *const u8 {
         self.font_subset.as_ptr()
+    }
+
+    fn process(&mut self) {
+        self.status = match self.status {
+            Status::Uninitialized => self.start_init_font_load(),
+            _ => todo!("Unimplemented!"),
+        };
+
+        if matches!(self.status, Status::Ready) {
+            self.notify_pending_callbacks()
+        }
+    }
+
+    fn notify_pending_callbacks(&mut self) {
+        todo!()
+    }
+
+    fn start_init_font_load(&mut self) -> Status {
+        todo!()
     }
 }
