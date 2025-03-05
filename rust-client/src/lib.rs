4a842f0726be0d834692548af55ea76a11c30304
Garret Rieger <grieger@google.com>
2025-03-05 00:17:09 +0000

    Hook up incremental-font-transfer and sketch the js<->rust interface.

--- rust-client/src/lib.rs
+++ rust-client/src/lib.rs
@@ -8,6 +8,33 @@ extern "C" {
 }
 
 #[wasm_bindgen]
-pub fn greet() {
-    alert("Hello, rust-client!");
+struct IftState {
+    font_subset: Vec<u8>,
+}
+
+#[wasm_bindgen]
+impl IftState {
+    pub fn new(font_url: &str) -> Self {
+        // TODO: implement me!
+        Self {
+            font_subset: vec![1, 2, 3, 4],
+        }
+    }
+
+    pub fn extend(&self, codepoints: &[u32], callback: &js_sys::Function) {
+        if !codepoints.is_empty() {
+            let this = JsValue::null();
+            let v = JsValue::from(codepoints[0]);
+            callback.call1(&this, &v);
+        }
+        // TODO: implement me!
+    }
+
+    pub fn font_size(&self) -> usize {
+        self.font_subset.len()
+    }
+
+    pub fn font_ptr(&self) -> *const u8 {
+        self.font_subset.as_ptr()
+    }
 }
